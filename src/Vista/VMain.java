2164ee20ac118e9fa69994c7e01f12f9932bdc60
RaulValEsc <raulvalesc@gmail.com>
2021-11-24 17:04:18 +0100

    .

--- src/Vista/VMain.java
+++ src/Vista/VMain.java
@@ -5,6 +5,10 @@
  */
 package Vista;
 
+import java.awt.event.KeyEvent;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author raulv
@@ -28,7 +32,21 @@ public class VMain extends javax.swing.JFrame {
     private void initComponents() {
 
         jSeparator1 = new javax.swing.JSeparator();
-        bCrear = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tPrestamos = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tRecibos = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        etNPrestamo = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        cbFormaDePago = new javax.swing.JComboBox<>();
+        etFecha = new javax.swing.JFormattedTextField();
+        bPagar = new javax.swing.JButton();
+        etImporte = new javax.swing.JFormattedTextField();
         jMenuBar1 = new javax.swing.JMenuBar();
         bmCrear = new javax.swing.JMenu();
         bmBorrar = new javax.swing.JMenu();
@@ -38,10 +56,100 @@ public class VMain extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        bCrear.setText("jButton1");
-        bCrear.addActionListener(new java.awt.event.ActionListener() {
+        jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
+
+        tPrestamos.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        tPrestamos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nº Prestamo", "Fecha", "Importe", "Importe Pagado", "Forma de Pago"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tPrestamos.setColumnSelectionAllowed(true);
+        tPrestamos.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        tPrestamos.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(tPrestamos);
+        tPrestamos.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        if (tPrestamos.getColumnModel().getColumnCount() > 0) {
+            tPrestamos.getColumnModel().getColumn(0).setResizable(false);
+            tPrestamos.getColumnModel().getColumn(1).setResizable(false);
+            tPrestamos.getColumnModel().getColumn(2).setResizable(false);
+            tPrestamos.getColumnModel().getColumn(3).setResizable(false);
+            tPrestamos.getColumnModel().getColumn(4).setResizable(false);
+        }
+
+        tRecibos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nº Recibo", "Fecha", "Importe", "Fecha de pago"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tRecibos.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        tRecibos.getTableHeader().setReorderingAllowed(false);
+        jScrollPane2.setViewportView(tRecibos);
+        if (tRecibos.getColumnModel().getColumnCount() > 0) {
+            tRecibos.getColumnModel().getColumn(0).setResizable(false);
+            tRecibos.getColumnModel().getColumn(1).setResizable(false);
+            tRecibos.getColumnModel().getColumn(2).setResizable(false);
+        }
+
+        jLabel1.setText("Recibos");
+
+        jLabel2.setText("Préstamos");
+
+        jLabel3.setText("Nº Prestamo");
+
+        jLabel4.setText("Fecha");
+
+        jLabel5.setText("Importe");
+
+        etNPrestamo.setEditable(false);
+        etNPrestamo.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        jLabel6.setText("Forma de pago");
+
+        cbFormaDePago.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        cbFormaDePago.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bCrearActionPerformed(evt);
+                cbFormaDePagoActionPerformed(evt);
+            }
+        });
+
+        etFecha.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        etFecha.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.DateFormatter(new java.text.SimpleDateFormat("dd/MM/yyyy"))));
+        etFecha.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                etFechaKeyTyped(evt);
+            }
+        });
+
+        bPagar.setText("Pagar");
+        bPagar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        etImporte.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("#0.00"))));
+        etImporte.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                etImporteKeyTyped(evt);
             }
         });
 
@@ -91,20 +199,72 @@ public class VMain extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jSeparator1)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(bCrear)
-                .addContainerGap(773, Short.MAX_VALUE))
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(bPagar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel3))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(etFecha)
+                            .addComponent(cbFormaDePago, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(etNPrestamo, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addComponent(etImporte))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(10, 10, 10)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 566, Short.MAX_VALUE)
+                        .addComponent(jScrollPane1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1))))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jSeparator1)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(bCrear)
-                .addGap(75, 75, 75)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(398, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 276, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(etNPrestamo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(26, 26, 26)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(etFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(33, 33, 33)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel5)
+                            .addComponent(etImporte, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(28, 28, 28)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel6)
+                            .addComponent(cbFormaDePago, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(42, 42, 42)
+                        .addComponent(bPagar)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -130,9 +290,25 @@ public class VMain extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_bmGenerarJSONActionPerformed
 
-    private void bCrearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bCrearActionPerformed
+    private void etImporteKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_etImporteKeyTyped
+        char num = evt.getKeyChar();
+        if (!Character.isDigit(num) && evt.getKeyChar() != KeyEvent.VK_BACK_SPACE && evt.getKeyChar() != KeyEvent.VK_COMMA) {
+            evt.consume();
+            getToolkit().beep();
+        }
+    }//GEN-LAST:event_etImporteKeyTyped
+
+    private void etFechaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_etFechaKeyTyped
+        char dig = evt.getKeyChar();
+        if (!Character.isDigit(dig) && evt.getKeyChar() != KeyEvent.VK_SLASH && evt.getKeyChar() != KeyEvent.VK_BACK_SPACE) {
+            evt.consume();
+            getToolkit().beep();
+        }
+    }//GEN-LAST:event_etFechaKeyTyped
+
+    private void cbFormaDePagoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbFormaDePagoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_bCrearActionPerformed
+    }//GEN-LAST:event_cbFormaDePagoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -170,13 +346,27 @@ public class VMain extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton bCrear;
+    private javax.swing.JButton bPagar;
     private javax.swing.JMenu bmBorrar;
     private javax.swing.JMenu bmCrear;
     private javax.swing.JMenu bmGenerarJSON;
     private javax.swing.JMenu bmModificar;
     private javax.swing.JMenu bmPagar;
+    private javax.swing.JComboBox<String> cbFormaDePago;
+    private javax.swing.JFormattedTextField etFecha;
+    private javax.swing.JFormattedTextField etImporte;
+    private javax.swing.JTextField etNPrestamo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JTable tPrestamos;
+    private javax.swing.JTable tRecibos;
     // End of variables declaration//GEN-END:variables
 }
